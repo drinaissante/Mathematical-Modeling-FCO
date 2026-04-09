2babbd0851b58177684686ac979c20b88a10ca58
katteeem <katteeem@gmail.com>
2026-04-09 20:18:54 +0800

    Added create_graph.m
    The create_graph.m MATLAB code creates and displays the graph needed for the visualization of results of the Genetic Algorithm. As the model creates the calculations needed for the optimal route, this helps us the maps of the nodes and the delivery paths, giving us the graph and the distance for each.

---
+++ src/create_graph.m
@@ -0,0 +1,36 @@
+function create_graph(run, expandedPath, nodeNames, costMatrix, positions, expandedEdges, totalDist)
+uniqueNodes = unique(expandedPath);
+subIdx = cellfun(@(x) find(strcmp(nodeNames,x)), uniqueNodes);
+subMatrix = costMatrix(subIdx, subIdx);
+subNames = nodeNames(subIdx);
+
+% extract positions for the subgraph nodes
+X = zeros(1,length(subIdx));
+Y = zeros(1,length(subIdx));
+for i = 1:length(subIdx)
+    coords = positions(subNames{i});
+    X(i) = coords(1);
+    Y(i) = coords(2);
+end
+
+% create figure (graph)
+figure;
+
+subG = graph(subMatrix, subNames, 'upper');
+h2 = plot(subG, 'XData',X , 'YData', Y, 'NodeLabel', subNames);
+h2.EdgeLabel = subG.Edges.Weight;
+
+for k = 1:size(expandedEdges, 1)
+    u = expandedEdges(k, 1);
+    v = expandedEdges(k, 2);
+
+    if ismember(u, subIdx) && ismember(v, subIdx)
+        highlight(h2, find(subIdx == u), find(subIdx == v), 'EdgeColor','r','LineWidth',3);
+    end
+end
+
+% highlight the matching route
+highlight(h2, 1:length(subIdx),'NodeColor','r','MarkerSize',7);
+
+title(sprintf('Run %d Clean Expanded Route (fixed positions), Distance = %.2f', run, totalDist));
+end
